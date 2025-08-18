f95fc17f13d85ca139d41b1f836589f91e396bec
luitvm <38038189+eltee1@users.noreply.github.com>
2025-08-18 14:11:59 +0200

    new m25 generated files after new habitat_areas delivery.

--- source/modules/src/data/sql/database-modules/nature_habitats_and_species/dataset_25.sql
+++ source/modules/src/data/sql/database-modules/nature_habitats_and_species/dataset_25.sql
@@ -1,6 +1,6 @@
 {import_common 'database-modules/nature_habitats_and_species/supplied_25.sql'}
 
 -- Generated data
-BEGIN; SELECT system.load_table('relevant_habitat_areas', '{data_folder}/common/nature_habitats_and_species/25/nature.relevant_habitat_areas_20250701.txt'); COMMIT;
+BEGIN; SELECT system.load_table('relevant_habitat_areas', '{data_folder}/common/nature_habitats_and_species/25/nature.relevant_habitat_areas_20250719.txt'); COMMIT;
 
 {import_common 'database-modules/nature_habitats_and_species/refresh_materialized_views.sql'}
